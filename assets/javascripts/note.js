f3bba8b780c42b837235be50ab4001b9f3497cf7
Tiou Lims <dourokinga@gmail.com>
2017-12-10 02:29:38 +0800

    美化笔记边栏

--- assets/javascripts/note.js
+++ assets/javascripts/note.js
@@ -5,17 +5,17 @@
  */
 const note = {
     navigator : function() {
-    function renderItem(parent, item, level = 1){
+    function renderItem(parent, item, level = 0){
         let	li = $("<li></li>") // 当前项
 	      let isActive  // 用于找到激活的菜单项
         if(item.children){
             li.addClass('bold')
 
-            let header = $("<a class=\"collapsible-header waves-effect waves-teal\">"+item.title+"</a>")
+            let header = $(`<a class="collapsible-header waves-effect waves-teal">${item.title}</a>`)
+
+	          header.addClass(`level-h${level}`);
             let body = $("<div class=\"collapsible-body\"></div>")
             let ul = $("<ul></ul>")
-
-	          ul.addClass('ul-h'+level);
             let grandchild = false;
 	          for(var i=0;i<item.children.length;i++){
                 grandchild |= !(item.children[i].children === "undefined");
@@ -29,13 +29,12 @@ const note = {
 	          }
             $(body).append(ul)
             li.append(header).append(body)
-
 	      }else{
             isActive = item.url == window.location.pathname
             if(isActive){
                 li.addClass('active')
             }
-            li.append("<a href='"+item.url+"'>"+item.title+"</a>")
+            li.append(`<a class="level-h${level}" href="${item.url}">${item.title}</a>`)
 	      }
         parent.append(li)
         return isActive
