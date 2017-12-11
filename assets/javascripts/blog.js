0964725828683aaac80e10034dfc523ad1520711
Tiou Lims <dourokinga@gmail.com>
2017-12-11 09:54:40 +0800

    优化 toc

--- assets/javascripts/blog.js
+++ assets/javascripts/blog.js
@@ -1,5 +1,46 @@
 ---
 ---
+!function($) {
+moon.toc = function(){
+        function findHeader(content){
+            return content.find("h1,h2,h3,h4,h5").map((_,header) => {
+                $(header).addClass("section scrollspy")
+                return {
+                    id    : header.id,
+                    title : header.innerText,
+                    level : ~~header.tagName[1]
+                }
+            })
+        }
+        function calcPushpin(){
+            let toc = $(".section.table-of-contents")
+            let wheight = $(window).height()
+            let tocTop = $('nav').height() + $(".ribbon").height()
+            let footerOffset = $('.page-footer').first().offset().top
+            let bottomOffset = footerOffset - toc.height()
+            console.log(`boffset${bottomOffset}`)
+            // Floating-Fixed table of contents
+            // 只有 toc 小于 window 高度才启用 pushpin
+            if(wheight > toc.height()){
+                toc.pushpin({
+                    top: tocTop,
+                    bottom: bottomOffset
+                });
+            }
+        }
+        return {
+            render: function(){
+                let headers = findHeader($(".content"))
+                let toc = $(".section.table-of-contents")
+                toc.empty()
+                headers.each(
+                    (_,h) => toc.append(`<li class="header-${h.level}"><a href="#${h.id}">${h.title}</li>`))
+                $(".section.scrollspy").scrollSpy()
+                calcPushpin()
+            }
+        }
+    }()
+}(jQuery)
 
 //fab
 $(function() {
@@ -17,11 +58,9 @@ $(function() {
     });
 
     function fireDown(){
-        console.log("down")
         $("#fab").addClass("scale-out")
     }
     function fireUp(){
-        console.log("up")
         $("#fab").removeClass("scale-out")
     }
 });
--- assets/javascripts/init.js
+++ assets/javascripts/init.js
@@ -41,42 +41,6 @@ const moon = {}
         $("#scroll-top").click(scrollToTop);
     })
 
-    moon.toc = function(){
-        function findHeader(content){
-            return content.find("h1,h2,h3,h4,h5").map((_,header) => {
-                $(header).addClass("section scrollspy")
-                return {
-                    id    : header.id,
-                    title : header.innerText,
-                    level : ~~header.tagName[1]
-                }
-            })
-        }
-        function calcPushpin(){
-            let toc = $(".section.table-of-contents")
-            let wheight = $(window).height()
-            let tocTop = $('nav').height() + $(".ribbon").height()
-            // Floating-Fixed table of contents
-            // 只有 toc 小于 window 高度才启用 pushpin
-            if(wheight > toc.height()){
-                toc.pushpin({
-                    top: tocTop
-                });
-            }
-        }
-        return {
-            render: function(){
-                let headers = findHeader($(".content"))
-                let toc = $(".section.table-of-contents")
-                toc.empty()
-                headers.each(
-                    (_,h) => toc.append(`<li class="header-${h.level}"><a href="#${h.id}">${h.title}</li>`))
-                $(".section.scrollspy").scrollSpy()
-                calcPushpin()
-            }
-        }
-    }()
-
 }(jQuery);
 
 
