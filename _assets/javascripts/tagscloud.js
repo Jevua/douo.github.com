78076027f57104f27210245dff87328d1c61488a
tiou <dourokinga@gmail.com>
2020-06-10 17:56:10 +0800

    fix: d3 升级到5.0. 不知为何回退到 4.0

--- _assets/javascripts/tagscloud.js
+++ _assets/javascripts/tagscloud.js
@@ -60,13 +60,10 @@ class TagsCloud {
   }
 
   setup() {
-      console.log("setup")
-      console.log(d3)
-      console.log(d3.json)
     if (this.data) {
       this.generate(this.data);
     } else {
-        d3.json('/tags.json', (data => {
+      d3.json('/tags.json').then(data => {
         this.data = data;
         this.generate(data);
         let w = window.innerWidth;
@@ -78,7 +75,7 @@ class TagsCloud {
             this.generate(this.data);
           }
         });
-        }));
+      });
     }
   }
 
